a93478d1eee2ac183e673b9bcd852ce98306d5d7
Abhishek <dekaa331@gmail.com>
2022-08-30 21:33:27 +0530

    add to card added

--- app.js
+++ app.js
@@ -25,11 +25,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.findById('630e208485c148e4f4ef4fca')
     .then(user => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch(err => console.log(err));
-  // next();
 });
 
 app.use('/admin', adminRoutes);
