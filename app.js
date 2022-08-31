d8122631bbcd121703debf42bc5587b2f6d5118c
Abhishek <dekaa331@gmail.com>
2022-08-31 14:34:48 +0530

    CRUD operations with mongoose

--- app.js
+++ app.js
@@ -1,42 +1,49 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const errorController = require('./controllers/error');
-const mongoConnect= require('./util/database').mongoConnect;
+const errorController = require("./controllers/error");
+const mongoose = require("mongoose");
 
-const dotenv= require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-const User= require('./models/user');
+const User = require("./models/user");
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use((req, res, next) => {
-  User.findById('630e208485c148e4f4ef4fca')
-    .then(user => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      next();
-    })
-    .catch(err => console.log(err));
-});
+// app.use((req, res, next) => {
+//   User.findById("630e208485c148e4f4ef4fca")
+//     .then((user) => {
+//       req.user = new User(user.name, user.email, user.cart, user._id);
+//       next();
+//     })
+//     .catch((err) => console.log(err));
+// });
 
-app.use('/admin', adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(()=>{
-  app.listen(3000);
-})
-
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MongoUserName}:${process.env.MongoPassword}@cluster0.epovlt4.mongodb.net/shop?retryWrites=true&w=majority`
+  )
+  .then(result=>{
+    app.listen(3000);
+    console.log('>>>>>>>>>connected')
+  })
+  .catch(err=>{
+    console.log(err);
+  });
